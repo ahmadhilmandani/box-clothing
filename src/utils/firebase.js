eac78be6f5ab6c0961050a5fef2d1b511b87a7d1
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-06-15 11:08:06 +0700

    ADD firebase config

---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAD0iTo7rhnhj9YTzM4ygtdWmyCqvkwRA",
+  authDomain: "box-clothing-db-4bdcf.firebaseapp.com",
+  projectId: "box-clothing-db-4bdcf",
+  storageBucket: "box-clothing-db-4bdcf.appspot.com",
+  messagingSenderId: "431483537321",
+  appId: "1:431483537321:web:0ce123525d08d9988a26b8"
+};
+
+// Initialize Firebase
+const firebaseInstanceApp = initializeApp(firebaseConfig);
